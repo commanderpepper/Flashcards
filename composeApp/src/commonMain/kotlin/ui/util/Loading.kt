c198b37f961652ff2e299cfaa4c9f935a0423d39
humza <ahmadhumza7@gmail.com>
2024-09-03 16:06:01 -0700

    Updated Loading UI

--- composeApp/src/commonMain/kotlin/ui/util/Loading.kt
+++ composeApp/src/commonMain/kotlin/ui/util/Loading.kt
@@ -1,18 +1,30 @@
 package ui.util
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Colors
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 fun Loading(modifier: Modifier = Modifier){
-    CircularProgressIndicator(
-        modifier = modifier.width(64.dp),
-        color = Color.Red
-    )
+    Column(modifier = modifier, verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
+        CircularProgressIndicator(
+            modifier = Modifier.width(32.dp),
+            color = Color.Red
+        )
+    }
+}
+
+@Preview
+@Composable
+fun LoadingPreview(){
+    Loading()
 }
